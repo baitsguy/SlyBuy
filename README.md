27dcbed7cdab9a52b58db1b7516af50e3eed9880
Vinayak Thapliyal <vinayakthapliyal@gmail.com>
2013-09-21 00:39:57 -0400

    Set up rails app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-tong
+Project Santa
 ====
+
+Ever wished you could do secret Santa and it's still June? Well now you can.
+
---
+++ santa/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Santa::Application.config.secret_key_base = 'ee5b43d44456efaa5ad6b794e98bb60913f7a7af1861633e7b8f3e686ef2080ea1631362cf6ca8c6266ffd6f6e487269e8b002764e18453213e2233896bd0a08'

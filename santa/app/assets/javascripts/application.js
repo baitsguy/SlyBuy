374451c6f021e30c0cf6da7c1752bf64ecc3b76b
Vinayak Thapliyal <vinayakthapliyal@gmail.com>
2013-09-22 07:32:35 -0400

    Setup venmo payment, giving an error currently

--- santa/app/assets/javascripts/application.js
+++ santa/app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require_tree .
 $(document).ready(function(){
+		$('#lightbox').hide();
 		$('#home').height(1000);
 		$('#friends').height(1000);
 		$('#charity').height(1000);
@@ -23,6 +24,16 @@ $(document).ready(function(){
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
     return false;
+		});
+		$('.charity').on('click',function() {
+				$('#lightbox').fadeIn("fast");
+				$('<input>').attr({type:'hidden',name:'charity',value:$(this).attr('id')}).appendTo('#charity_form');
+		});
+		$('body').click(function(e) {
+				if(e.target.id == 'lightbox' || $('#lightbox').has(e.target).length != 0)
+						return true;
+				else 
+						$('#lightbox').fadeOut('fast');
 		});
   $('html,body').animate({scrollTop: $('#home').offset().top},'slow');
 
